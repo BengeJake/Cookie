7658b94c3f5d62f6e7bfa771870350745698430f
Jake <104634523+BengeJake@users.noreply.github.com>
2022-05-06 15:37:47 +0100

    rain innit

---
+++ partical.js
@@ -0,0 +1,52 @@
+
+const screenWidth = 900
+const screenHeight = 900
+let drop
+let numberOfDrops = 900
+let drops = []
+let colour = '#8FF'
+
+
+function setup() {
+  drop = new RainDrop()
+  createCanvas(screenWidth, screenHeight);
+  for(let i=0; i<numberOfDrops; i++){
+    drops.push(new RainDrop())
+  }
+}
+
+function draw() {
+  background(0);
+  for(let i =0; i < drops.length;i++){
+    drops[i].renderRainDrop()
+  }
+  
+  
+}
+//creates intances of rain
+class RainDrop{
+  constructor(){
+    this.x = random(0, screenWidth)
+    this.y = random(0, screenHeight)
+    this.speed = random(12,4)
+    this.width = random(1, 3)
+    this.height = random(3, 6)
+  }
+  //sets spawn
+  renderRainDrop(){
+    this.drop()
+    stroke(colour)
+    rect(this.x, this.y, this.width, this.height)
+  }
+  //sets animation
+  drop(){
+    this.x += this.speed
+    this.y += this.speed
+    if(this.x > screenHeight){
+      this.x = -5
+    }
+    if(this.y > screenWidth){
+      this.y = -5
+    }
+  }
+}
