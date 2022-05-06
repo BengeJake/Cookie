88e470aa1f6bc3b8da1c9865dc8032e53b7371f2
Jake <104634523+BengeJake@users.noreply.github.com>
2022-05-06 12:55:18 +0100

    bovril

--- test.js
+++ test.js
@@ -2,16 +2,24 @@ function setup() {
     let playing = true
     let answer = 5
     let message = 'Guess a number'
+
     while(playing){
+        
         let guess = Number(prompt(message))
-       
-        if(guess == answer){
-            playing = false
-        }else if(guess > answer){
-            message = 'Too high!'
-        }else{
-            message = 'Too low!'
-        }  
+        for(let attempts = 1; attempts <= 3; attempts++){
+
+            if(guess == answer){
+                playing = false
+            }else if(guess > answer){
+                message = 'Too high!'
+            }else{
+                message = 'Too low!'
+            }  
+            if (attempts > 3){
+                break;
+            }
+        }
+        
     }
     alert("YOU WON")
 }
