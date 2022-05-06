9a860346325be8f93ea325acee793000b665ade5
Jake <104634523+BengeJake@users.noreply.github.com>
2022-05-06 13:59:03 +0100

    wrks better

--- test.js
+++ test.js
@@ -5,21 +5,21 @@ function setup() {
 
     while(playing){
         
-        let guess = Number(prompt(message))
+        //let guess = Number(prompt(message))
         for(let attempts = 1; attempts <= 3; attempts++){
+            let guess = Number(prompt(message))
 
-            if(guess == answer){
-                playing = false
-            }else if(guess > answer){
-                message = 'Too high!'
-            }else{
-                message = 'Too low!'
-            }  
             if (attempts > 3){
                 break;
             }
         }
-        
+        if(guess == answer){
+            playing = false
+        }else if(guess > answer){
+            message = 'Too high!'
+        }else{
+            message = 'Too low!'
+        }
     }
     alert("YOU WON")
-}
+} 
